43204b278c6504a62efdb35609ff81905157861b
zqf <zqf1@vip.qq.com>
2018-01-15 23:41:47 +0800

    111

---
+++ zqf/test_1/test_1_4.py
@@ -0,0 +1,34 @@
+print('I\'m ok');
+
+print('I\'m learning\nPython.');
+
+print('\\\t\\');
+
+print(r'\\\t\\');#不转义
+
+print('''1\///''');#不转义
+
+print('sdfsfsfss.....'
+      '.fsfsfa..'
+      '..fadf');
+
+x=10;
+y=x+2;
+print(y);
+a='fds';
+b=a;
+a='ddsafs';
+print(b);
+n=123;
+f=456.789;
+s1='Hello, Word';
+s2='Hello, \'Adam\'';
+s3=r'Hello,"Bart"';
+s4=r'''Hello,
+Lisa''';
+print(n);
+print(f);
+print(s1);
+print(s2);
+print(s3);
+print(s4);
